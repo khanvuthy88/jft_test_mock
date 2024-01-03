67e13730cc1e59daa810e5ab99bf046c7b76ef39
khanvuthy88 <khanvuthy88@gmail.com>
2024-01-03 17:42:24 +0700

    Update controller

--- jft_mock_test/controllers/controllers.py
+++ jft_mock_test/controllers/controllers.py
@@ -7,35 +7,86 @@ from odoo.http import request, content_disposition
 class JFTSurvey(Survey):
 
     def _prepare_question_html(self, survey_sudo, answer_sudo, **post):
+        """
+        Prepare HTML content for survey questions.
+
+        This method enhances the HTML content for survey questions by integrating
+        additional data such as section titles and aside information. It utilizes
+        the provided survey_sudo, answer_sudo, and additional post parameters to
+        create a structured response.
+
+        Args:
+            survey_sudo (recordset): Survey record.
+            answer_sudo (recordset): Survey answer record.
+            **post: Additional parameters from the HTTP request.
+
+        Returns:
+            dict: A dictionary containing the prepared HTML content with added
+                  details about section titles and aside information.
+        """
+
         res = super()._prepare_question_html(survey_sudo, answer_sudo, **post)
         survey_data = self._prepare_survey_data(survey_sudo, answer_sudo, **post)
+
         jft_section_title = request.env['ir.qweb']._render('jft_mock_test.jft_survey_section_title', survey_data)
+        jft_section_aside = request.env['ir.qweb']._render('jft_mock_test.jft_survey_aside', survey_data)
+
         res['jft_section_title'] = jft_section_title
+        res['jft_section_aside'] = jft_section_aside
+
         return res
 
     def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
+        """
+        Prepare survey data for rendering.
+
+        This method enhances the prepared survey data by including information
+        about active sections and their questions. It utilizes the provided
+        survey_sudo, answer_sudo, and additional post parameters to create
+        a structured response.
+
+        Args:
+            survey_sudo (recordset): Survey record.
+            answer_sudo (recordset): Survey answer record.
+            **post: Additional parameters from the HTTP request.
+
+        Returns:
+            dict: A dictionary containing the prepared survey data with added
+                  details about sections and questions.
+        """
         res = super()._prepare_survey_data(survey_sudo, answer_sudo, **post)
         jft_section_question = {}
         section_title = ''
         actived_section = []
+
+        if 'active_session_id' not in res:
+            res['active_session_id'] = 0
+
         if 'question' in res:
             section_title = res['question'].title
-            actived_section.append(res['question'] and res['question'].is_page and res['question'].title)
+            if res['question'].is_page:
+                res['active_session_id'] = res['question'].id
         for section in survey_sudo.page_ids:
             jft_section_name = section.title
+
             if jft_section_name not in actived_section:
                 actived_section.append(jft_section_name.title)
-            if section.title not in jft_section_question:
+
+            if jft_section_name not in jft_section_question:
                 jft_section_question[jft_section_name] = []
+
             for question in section.question_ids:
+                is_active_question = 'question' in res and question.id == res['question'].id
                 jft_section_question[jft_section_name].append({
-                    'active': question.id == 'question' in res and res['question'].id,
+                    'active': is_active_question,
                     'title': question.title,
                     'id': question.id,
                     'section': section.title
                 })
-                if 'question' in res and question.title == res['question'].title:
+
+                if is_active_question:
                     section_title = section.title
+
         res['jft_section'] = jft_section_question
         res['jft_section_title'] = section_title
         res['active_section'] = actived_section
@@ -45,3 +96,8 @@ class JFTSurvey(Survey):
     @http.route()
     def survey_begin(self, survey_token, answer_token, **post):
         return super().survey_begin(survey_token, answer_token, **post)
+
+    @http.route()
+    def survey_submit(self, survey_token, answer_token, **post):
+        res = super().survey_submit(survey_token, answer_token, **post)
+        return res
--- jft_mock_test/static/src/js/survey_form.js
+++ jft_mock_test/static/src/js/survey_form.js
@@ -4,8 +4,13 @@ import SurveyFormWidget from '@survey/js/survey_form';
 
 SurveyFormWidget.include({
     _onNextScreenDone(options) {
-        this._super(...arguments);
         var result = this.nextScreenResult;
+        if(result.jft_section_aside){
+            $('.jft_survey_aside').empty();
+            $('.jft_survey_aside').html(result.jft_section_aside);
+        }
+        this._super(...arguments);
+
         if(result.jft_section_title){
             $('section#jft_sub_title').empty();
             $('section#jft_sub_title').html(result.jft_section_title);
